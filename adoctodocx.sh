d7da426e9e2160574461c0ff021307dc3f5c740f
Victor Gregorio <victor.gregorio@suse.com>
2022-10-05 12:04:39 -0400

    Added comments

--- adoctodocx.sh
+++ adoctodocx.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#
+# SLE 15 Requirements: ruby2.5-rubygem-asciidoctor, pandoc
+#
+# Reference
 # https://rmoff.net/2020/04/16/converting-from-asciidoc-to-google-docs-and-ms-word/
+#
 
 INPUT_ADOC=${1:?}
 
